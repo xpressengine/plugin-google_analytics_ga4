369964e314f579ac97596ed650a233ac40724d77
parkjg20 <parkjg20@naver.com>
2022-09-15 15:25:21 +0900

    gtag script load 시 id 인자 수정

--- views/tracking.blade.php
+++ views/tracking.blade.php
@@ -1,5 +1,5 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F97PPWQ2Z2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $measurementId }}"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
